17695a5a7662d577d83fc507e83a72cf503991ab
Aline <127116322+allsme@users.noreply.github.com>
2024-02-16 10:38:16 -0300

    Update Cozzinhe_reduzido.sql

--- Cozzinhe_reduzido.sql
+++ Cozzinhe_reduzido.sql
@@ -15,7 +15,6 @@ CREATE TABLE RecipeIngredients (
     id_recipeingredients INT PRIMARY KEY,-- Chave primária para identificar exclusivamente cada entrada de ingrediente da receita
     id_recipes INT,                      -- Chave estrangeira referenciando a tabela de Recipes, indicando a qual receita o ingrediente pertence
     id_ingredients INT,                  -- Chave estrangeira referenciando a tabela de Ingredients, indicando qual ingrediente está sendo usado
-    quantity VARCHAR(50),                -- Quantidade do ingrediente necessário para a receita
     FOREIGN KEY (id_recipes) REFERENCES Recipes(id_recipes),-- Restrição de chave estrangeira para garantir a integridade referencial
     FOREIGN KEY (id_ingredients) REFERENCES Ingredients(id_ingredients)-- Restrição de chave estrangeira para garantir a integridade referencial
 );
