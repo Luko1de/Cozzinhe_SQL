27b04334d0510dfed62555e30a66b935dd20e576
Aline <127116322+allsme@users.noreply.github.com>
2024-02-19 23:26:23 -0300

    Update banco_dados.sql

--- crud_cozzinhe/models/banco_dados.sql
+++ crud_cozzinhe/models/banco_dados.sql
@@ -8,12 +8,16 @@ CREATE TABLE Recipes (
     descricao TEXT,                     -- Descrição da receita
     quantity VARCHAR(50)                -- Quantidade do ingrediente necessário para a receita
 );
+
 -- Tabela para armazenar os ingredientes
+
 CREATE TABLE Ingredients (
     id_ingredients INT PRIMARY KEY,     -- Chave primária para identificar exclusivamente cada ingrediente
     nome VARCHAR(255)                   -- Nome do ingrediente
 );
+
 -- Tabela para armazenar os ingredientes de cada receita
+
 CREATE TABLE RecipeIngredients (
     id_recipeingredients INT PRIMARY KEY,-- Chave primária para identificar exclusivamente cada entrada de ingrediente da receita
     id_recipes INT,                      -- Chave estrangeira referenciando a tabela de Recipes, indicando a qual receita o ingrediente pertence
@@ -21,13 +25,16 @@ CREATE TABLE RecipeIngredients (
     FOREIGN KEY (id_recipes) REFERENCES Recipes(id_recipes),-- Restrição de chave estrangeira para garantir a integridade referencial
     FOREIGN KEY (id_ingredients) REFERENCES Ingredients(id_ingredients)-- Restrição de chave estrangeira para garantir a integridade referencial
 );
+
 CREATE TABLE cozzinhe_import (
     id INT PRIMARY KEY,
     name VARCHAR(255),
     ingredients TEXT,
     n_ingredients INT
 );
+
 -- Trigger para evitar ingredientes duplicados
+
 CREATE TRIGGER evitar_ingredientes_duplicados 
 BEFORE INSERT ON recipeingredients 
 FOR EACH ROW
@@ -41,6 +48,7 @@ BEGIN
 END;
 
 -- Trigger para evitar receitas duplicadas
+
 CREATE TRIGGER evitar_receitas_duplicadas 
 BEFORE INSERT ON recipes 
 FOR EACH ROW
