0135705491258c1f0dbc2e4f22dab0b7bea5bedb
Layane <lane.silva.int@gmail.com>
2024-02-21 12:59:11 -0300

    atual

--- crud_cozzinhe/models/banco_dados.sql
+++ crud_cozzinhe/models/banco_dados.sql
@@ -23,7 +23,7 @@ CREATE TABLE RecipeIngredients (
     id_recipeingredients INT PRIMARY KEY,-- Chave primária para identificar exclusivamente cada entrada de ingrediente da receita
     id_recipes INT,                      -- Chave estrangeira referenciando a tabela de Recipes, indicando a qual receita o ingrediente pertence
     id_ingredients INT,                  -- Chave estrangeira referenciando a tabela de Ingredients, indicando qual ingrediente está sendo usado
-    FOREIGN KEY (id_recipe) REFERENCES recipes(id_recipe) ON DELETE CASCADE,-- Restrição de chave estrangeira para garantir a integridade referencial
+    FOREIGN KEY (id_recipes) REFERENCES recipes(id_recipes) ON DELETE CASCADE,-- Restrição de chave estrangeira para garantir a integridade referencial
     FOREIGN KEY (id_ingredients) REFERENCES Ingredients(id_ingredients) ON DELETE CASCADE-- Restrição de chave estrangeira para garantir a integridade referencial
 );
 
