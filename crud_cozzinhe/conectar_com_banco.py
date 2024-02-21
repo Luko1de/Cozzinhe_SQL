f5d743c4f4162368cb8e8e71ff9f4489316f0dbd
Aline <127116322+allsme@users.noreply.github.com>
2024-02-21 08:36:22 -0300

    Update conectar_com_banco.py

--- crud_cozzinhe/conectar_com_banco.py
+++ crud_cozzinhe/conectar_com_banco.py
@@ -1,9 +1,9 @@
 import mysql.connector
 import streamlit as st
 conexao = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="123456",
+            host="localhost", #Trocar o host para a sua
+            user="root", #Trocar o user para o seu, ou pode deixar o root
+            password="123456", #Trocar a senha para a sua
             database="Cozzinhe"
         )
 cursor = conexao.cursor()
